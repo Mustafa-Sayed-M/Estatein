6a48fc7b729d2a2b76de4f950dcff75ad0278510
Mustafa sayed <mustafacoder9@gmail.com>
2025-06-13 18:33:19 +0300

    Edit header hero path

--- src/Components/Header/Components/HeaderHero.jsx
+++ src/Components/Header/Components/HeaderHero.jsx
@@ -2,7 +2,7 @@ function HeaderHero() {
     return (
         <div className="header-hero max-lg:rounded-md max-lg:overflow-hidden max-lg:border max-lg:border-grey-15">
             <img
-                src="/public/assets/images/header-hero.png"
+                src="/assets/images/header-hero.png"
                 alt="Header Hero"
             />
         </div>
